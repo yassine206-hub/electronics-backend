b4c0f2f208dafc16db55adea7bed84d87639334c
Yassine Afi <yassineafi31@gmail.com>
2026-04-01 01:36:00 +0100

    fix cors for netlify

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,12 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'https://silver-pothos-b43b1e.netlify.app',
+        'https://harmonious-malabi-226c3f.netlify.app',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
